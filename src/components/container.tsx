51f366cca897360c4e2e44eb6c676c1d2df28dd6
2wons <013ayu@gmail.com>
2024-08-27 21:37:54 +0800

    improve semantics

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -2,10 +2,10 @@ import { ReactNode } from "react";
 
 export const Container = ({ children }: { children?: ReactNode }) => {
   return (
-    <div className="items-center py-2 pt-10 bg-gradient-to-b from-[#1e242c] via-[#14181c] to-[#14181c] bg-no-repeat">
+    <main className="items-center py-2 pt-10 bg-gradient-to-b from-[#1e242c] via-[#14181c] to-[#14181c] bg-no-repeat">
       <section className="mx-4 lg:mx-52 items-center relative">
         {children}
       </section>
-    </div>
+    </main>
   );
 };
