a19e79674511a3d30296610aa77bd87532210a71
2wons <013ayu@gmail.com>
2024-08-27 23:55:22 +0800

    feat: add show more/less button to actor overview

--- src/routes/Actor.tsx
+++ src/routes/Actor.tsx
@@ -7,6 +7,7 @@ import { useParams } from "react-router-dom";
 
 const Actor = () => {
   const [person, setPerson] = useState<PersonResponse>();
+  const [showMore, setShowMore] = useState(false);
   const { actorid } = useParams();
 
   useEffect(() => {
@@ -26,7 +27,7 @@ const Actor = () => {
   return (
     <Container>
       <div className="md:grid grid-cols-3 gap-10">
-        <div className="col-span-2">
+        <section className="col-span-2">
           <p className="text-muted-foreground text-md">Films Starring</p>
           <h1 className="font-bold text-2xl pb-5">{person?.name}</h1>
           <div className="flex border-y-2 border-gray-700 p-1.5 justify-between text-muted-foreground text-sm mb-3">
@@ -41,17 +42,31 @@ const Actor = () => {
             films={person?.movie_credits.cast || []}
             minWidth={150}
           />
-        </div>
-        <div className="flex md:flex-col md:w-full py-8 md:py-0 space-x-5 md:space-x-0 items-start">
+        </section>
+        <section className="flex md:flex-col md:w-full py-8 md:py-0 space-x-5 md:space-x-0 items-start">
           <img
             src={image(person?.profile_path!)}
             alt="actor_pic"
             className="rounded-lg w-1/3 md:w-auto object-scale-down"
           />
-          <p className="text-muted-foreground text-sm md:py-2">
-            {person?.biography}
-          </p>
-        </div>
+          <article>
+            <p
+              className={`text-muted-foreground text-sm md:pt-2 ${
+                !showMore && "line-clamp-5"
+              }`}
+            >
+              {person?.biography}
+            </p>
+            <span>
+              <button
+                onClick={() => setShowMore(!showMore)}
+                className="text-sky-500"
+              >
+                {showMore ? "Show less" : "Show more"}
+              </button>
+            </span>
+          </article>
+        </section>
       </div>
     </Container>
   );
